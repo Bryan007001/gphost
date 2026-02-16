9e05e19955bde574d53079babd39b3889ee80a93
root <root@ser121257671411.local>
2026-02-16 05:10:31 +0000

    split large files

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e -x
 
+cat lyasper-sshd-centos7.tar.gz.part_* > lyasper-sshd-centos7.tar.gz
 docker image load -i ./lyasper-sshd-centos7.tar.gz
+rm -f lyasper-sshd-centos7.tar.gz
+
+cat rpms.tar.gz.part_* > rpms.tar.gz
 docker build . -t lyasper/gphost
+rm -f rpms.tar.gz
